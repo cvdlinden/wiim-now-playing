76d08a99dfdc35be1cd8c65f42990fc1c2bc40f6
Caspar van der Linden <c.vanderlinden@rubicon.nl>
2025-02-05 02:05:09 +0100

    copilot review of ssdp

--- server/lib/ssdp.js
+++ server/lib/ssdp.js
@@ -4,12 +4,12 @@
 
 /**
  * SSDP functionality module.
- * The processes are asynchronous and take a wile, so wait for results to come in.
+ * The processes are asynchronous and take a while, so wait for results to come in.
  * @module
  */
 
 // Use SSDP module
-const SSDP = require("node-ssdp").Client
+const SSDP = require("node-ssdp").Client;
 
 // Other modules
 const upnp = require("./upnpClient.js"); // UPnP Client functionality
@@ -28,32 +28,34 @@ const scan = (deviceList, serverSettings) => {
     deviceList.length = 0;
 
     // Create client
-    const ssdpClient = new SSDP({ explicitSocketBind: true }); // explicitSocketBind enabled to make it work on Windows 11
+    const ssdpClient = new SSDP({ explicitSocketBind: true }); // explicitSocketBind enabled to ensure compatibility with Windows 11
 
     // Event listener on responses from device discovery
     ssdpClient.on("response", (respSSDP, code, rinfo) => {
         // log("Fetching device info for:", respSSDP.LOCATION);
 
-        if (deviceList.some((d) => { return d.location === respSSDP.LOCATION })) {
-            log("Device already found!"); // No need to add this device
+        if (deviceList.some((d) => d.location === respSSDP.LOCATION)) {
+            log("Device already found at:", respSSDP.LOCATION); // No need to add this device
         }
         else {
-            log("New device found! Getting details...");
+            log("New device found at:", respSSDP.LOCATION, "Getting details...");
             // log("Get the device description for:", respSSDP.LOCATION);
             upnp.getDeviceDescription(deviceList, serverSettings, respSSDP);
-        };
+        }
 
     });
 
     // Start a UPnP/DLNA search
-    // ssdpClient.search('ssdp:all'); // Search all devices
-    // ssdpClient.search("urn:schemas-upnp-org:device:MediaRenderer"); // Search for MediaRenderer devices
-    ssdpClient.search("urn:schemas-upnp-org:service:AVTransport:1"); // Search for AVTransport enabled devices
+    // const searchTarget = "ssdp:all"; // Search all devices
+    // const searchTarget = "urn:schemas-upnp-org:device:MediaRenderer"; // Search for MediaRenderer devices
+    const searchTarget = "urn:schemas-upnp-org:service:AVTransport:1"; // Search target for AVTransport enabled devices
 
     // OpenHome. Discovery works, but talking to devices is different! -> TODO
     // There is an npm package: https://www.npmjs.com/package/openhome-devices-manager
-    // ssdpClient.search("urn:av-openhome-org:service:Product:1"); // Search for OpenHome devices
-    // ssdpClient.search("urn:av-openhome-org:service:Product:2"); // Search for OpenHome devices
+    // const searchTarget = "urn:av-openhome-org:service:Product:1"; // Search for OpenHome devices
+    // const searchTarget = "urn:av-openhome-org:service:Product:2"; // Search for OpenHome devices
+    
+    ssdpClient.search(searchTarget);
 
 }
 
